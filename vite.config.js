032def47f5f36ca6da1cbaba69acafac4c7108e6
shashi8466 <shashikumaredula@mail.com>
2026-06-03 19:18:13 +0530

    chore: change local backend port to 3002 to avoid local conflict

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     historyApiFallback: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:3002',
         changeOrigin: true,
         secure: false,
       },
